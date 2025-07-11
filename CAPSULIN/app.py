5cfb46b293d9600fd9a91d00eb12c370454344a2
Txkken-trydev <alejandrocabrera2603@gmail.com>
2025-07-11 13:53:30 -0400

    match

--- CAPSULIN/app.py
+++ CAPSULIN/app.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template, request, jsonify
 from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.logic import BestMatch
 import sqlite3
 
 app = Flask(__name__)
 
-# Configura ChatterBot
+# Configurar ChatterBot
 chatbot = ChatBot("Capsulin", read_only=True)
 trainer = ChatterBotCorpusTrainer(chatbot)
 trainer.train("chatterbot.corpus.spanish")
-
+logic_adapters=[
+        {
+            "import_path": "chatterbot.logic.BestMatch",
+            "default_response": "Lo siento, no entendí eso.",
+            "maximum_similarity_threshold": 0.80
+        }
+    ]
 # Función para consultar SQLite
 def consultar_por_componente(componente):
     conn = sqlite3.connect("medicamentos.db")
